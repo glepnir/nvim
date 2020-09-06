38cc4b9a513331a0410cc32f8684a0ae2697e4e5
Raphael <glephunter@gmail.com>
2020-09-06 23:56:49 +0800

    TODO: rewrite install scripts

--- lua/lspconf.lua
+++ lua/lspconf.lua
@@ -1,3 +1,4 @@
+require 'global'
 local server = {}
 local vim = vim
 
@@ -16,7 +17,7 @@ server.go = {
 
 server.lua = {
   name = "lualsp";
-  cmd = {"/Users/stephen/lua-language-server/bin/macOS/lua-language-server", "-E", "/Users/stephen/lua-language-server/main.lua"};
+  cmd = { home.."/lua-language-server/bin/macOS/lua-language-server", "-E", home.."/lua-language-server/main.lua"};
   filetypes = {'lua'};
   root_patterns = {'.git'};
 }
@@ -43,10 +44,11 @@ server.Dockerfile = {
 }
 
 local lsp_intall_scripts = [=[
+# Install gopls
 cd $HOME
 go get golang.org/x/tools/gopls@latest
 
-# clone project
+# clone lua lsp and build it
 git clone https://github.com/sumneko/lua-language-server
 cd lua-language-server
 git submodule update --init --recursive
@@ -56,20 +58,27 @@ ninja -f ninja/macos.ninja
 cd ../..
 ./3rd/luamake/luamake rebuild
 
+# Install dockerfile lsp server
 npm install -g dockerfile-language-server-nodejs
+
+# Install bash lsp server
 npm i -g bash-language-server
 
+# Install rust-analyzer
 RUSTANALYZER=/tmp/rust-analyzer
-
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o $RUSTANALYZER
-
 chmod +x $RUSTANALYZER
-
 mv $RUSTANALYZER /usr/local/bin/
+
 ]=]
 
-function lsp_install_server()
+-- TODO: check install scripts and design a better install function
+function server.lsp_install_server()
   os.execute("sh -c"..lsp_intall_scripts)
+end
+
+function server.install_command()
+    vim.api.nvim_command("command! -nargs=0 -bar LspInstall lua require'lspconf'.lsp_install_server()")
 end
 
 return server
