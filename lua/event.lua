3e82abec4f26e17c2439fd2a0accd4c8ba3ab764
Raphael <glepnir@gopherhub.org>
2020-09-07 00:31:51 +0800

    refactor

--- lua/event.lua
+++ lua/event.lua
@@ -41,14 +41,13 @@ function autocmd.load_autocmds()
 
     ft = {
       {"FileType", "dashboard", "set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2"};
-      {"BufNewFile,BufRead", "coc-settings.json" ,"setlocal filetype=jsonc"};
     };
 
     yank = {
       {"TextYankPost", [[* silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=400})]]};
     };
     lsp= {
-      {"InsertEnter","*","lua require'lspinit'.start_lsp_server()"};
+      {"InsertEnter","*","lua require'lsp.lspinit'.start_lsp_server()"};
     };
   }
 
