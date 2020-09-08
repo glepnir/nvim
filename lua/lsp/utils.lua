c7a07264fb67c14c9b4595d4a754edbafe404166
Raphael <glephunter@gmail.com>
2020-09-08 23:43:54 +0800

    add peek_references

--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -1,18 +1,23 @@
-local vim = vim
+local vim,api = vim,vim.api
 local M = {}
 
-function M.lsp_float_window()
+function M.create_float_window(contents,opts)
+  opts = opts or {}
+  -- Clean up input: trim empty lines from the end, pad
+  contents = vim.lsp.util._trim_and_pad(contents, opts)
   -- get the editor's max width and height
-  local width = vim.api.nvim_get_option("columns")
-  local height = vim.api.nvim_get_option("lines")
+  local width = api.nvim_get_option("columns")
+  local height = api.nvim_get_option("lines")
 
   -- create a new, scratch buffer, for fzf
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+  local floating_bufnr = vim.api.nvim_create_buf(false, true)
+  local floating_winnr = nil
+  api.nvim_buf_set_lines(floating_bufnr, 0, -1, true, contents)
+  api.nvim_buf_set_option(floating_bufnr, 'filetype', 'markdown')
+  api.nvim_buf_set_option(floating_bufnr, 'modifiable', false)
 
   -- if the editor is big enough
   if (width > 150 or height > 35) then
-    -- fzf's window height is 3/4 of the max height, but not more than 30
     local win_height = math.min(math.ceil(height * 3 / 4), 30)
     local win_width
 
@@ -25,9 +30,10 @@ function M.lsp_float_window()
       win_width = math.ceil(width * 0.9)
     end
 
-    -- settings for the fzf window
+    -- settings for the float window
     local opts = {
-      relative = "editor",
+      relative = "cursor",
+      style = "minimal",
       width = win_width,
       height = win_height,
       row = math.ceil((height - win_height) / 2),
@@ -35,8 +41,9 @@ function M.lsp_float_window()
     }
 
     -- create a new floating window, centered in the editor
-    local win = vim.api.nvim_open_win(buf, true, opts)
+    floating_winnr = vim.api.nvim_open_win(floating_bufnr, true, opts)
   end
+  return floating_bufnr,floating_winnr
 end
 
 return M
