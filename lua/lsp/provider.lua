797537ac4fd08205fa073e78e0e956ec713e3094
Raphael <glepnir@gopherhub.org>
2020-09-08 23:43:54 +0800

    add peek_references

--- lua/lsp/provider.lua
+++ lua/lsp/provider.lua
@@ -1,3 +1,5 @@
+local global = require 'global'
+local utils = require 'lsp.utils'
 local vim,api = vim,vim.api
 local M = {}
 
@@ -35,9 +37,6 @@ local function preview_location_callback(_, method, result)
   end
 end
 
-function M.lsp_peek_references(_,method,result)
-end
-
 function M.lsp_peek_definition()
   if vim.tbl_contains(vim.api.nvim_list_wins(), M.floating_win) then
       vim.api.nvim_set_current_win(M.floating_win)
@@ -47,7 +46,51 @@ function M.lsp_peek_definition()
   end
 end
 
+local function references_preview_location_callback(_,method,result)
+  if result == nil or vim.tbl_isempty(result) then
+    print("No Location found:" .. method)
+    return nil
+  end
+  if vim.tbl_islist(result) then
+    local uri = result[1].targetUri or result[1].uri
+    if uri == nil then
+        return
+    end
+    local bufnr = vim.uri_to_bufnr(uri)
+    if not vim.api.nvim_buf_is_loaded(bufnr) then
+      vim.fn.bufload(bufnr)
+    end
 
+    local files = {}
+    for index,_ in ipairs(result) do
+      files[index] = vim.uri_to_fname(result[index].uri)
+    end
+    local lines = {}
+    for index,_ in ipairs(result) do
+      local range = result[index].targetRange or result[index].range
+      lines[index] = api.nvim_buf_get_lines(bufnr,range.start.line-0,range["end"].line+1+5,false)
+    end
+    local title = 'References:'
+    local contents = {title}
+    -- TODO:better contents
+    for k,_ in ipairs(files) do
+      table.insert(contents,files[k])
+      if type(lines[k]) == table then
+        for a,_ in ipairs(lines[k]) do
+          table.insert(contents,lines[a])
+        end
+      end
+    end
+    print(global.dump(lines))
+
+    M.floatng_buf,M.floating_win = utils.create_float_window(contents)
+  end
+end
+
+function M.lsp_peek_references()
+  local params = vim.lsp.util.make_position_params()
+  return vim.lsp.buf_request(0,"textDocument/references",params,references_preview_location_callback)
+end
 
 -- jump to definition in split window
 function M.lsp_jump_definition()
