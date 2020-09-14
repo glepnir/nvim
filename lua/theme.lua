e500832e62f5a4e0fb7922be0864c55824b01850
Raphael <glephunter@gmail.com>
2020-09-14 17:38:10 +0800

    rebuild

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,13 +1,13 @@
 local global = require 'global'
 local vim,api = vim,vim.api
 local theme = {}
-local theme_cache = global.cache_dir..global.path_sep..'theme.txt'
+local theme_cache = global.cache_dir..'theme.txt'
 
 function theme.load_theme()
   local default_theme = 'oceanic_material'
   if vim.g.colors_name == nil then
     api.nvim_set_option('background','dark')
-    local scheme = vim.fn.filereadable(theme_cache) and vim.fn.readfile(theme_cache)[1] or default_theme
+    local scheme = vim.fn.filereadable(theme_cache) and default_theme or vim.fn.readfile(theme_cache)[1]
     api.nvim_command("colorscheme "..scheme)
   end
   api.nvim_command("augroup LoadTheme")
