ac1a78e7504f2d0bf85b23ff2d3c2cd5d38c3f96
Raphael <glepnir@gopherhub.org>
2020-09-15 21:48:58 +0800

    add person hack quickrun and remove quickrun plugin

---
+++ lua/quickrun.lua
@@ -0,0 +1,26 @@
+local global = require 'global'
+local vim = vim
+local M = {}
+
+do
+  M.go = {'go run ','go test '}
+end
+
+function M.run_command()
+  local cmd = nil
+  local file_extension = vim.fn.expand("%:e")
+  local file_name = vim.fn.expand("%:p")
+  if file_extension == 'go' then
+    if file_name:match("_test") then
+      cmd = M[file_extension][2]
+    else
+      cmd = M[file_extension][1]
+    end
+    local output_list = vim.fn.split(vim.fn.system(cmd..file_name),'\n')
+    for _,v in ipairs(output_list) do
+      print(v)
+    end
+  end
+end
+
+return M
