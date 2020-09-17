6b3963262cc21b020e9ecf5d6376299181d26645
Raphael <glepnir@gopherhub.org>
2020-09-17 21:33:14 +0800

    rewrite

--- lua/lsp/provider.lua
+++ lua/lsp/provider.lua
@@ -197,5 +197,35 @@ function M.lsp_jump_definition()
   end
 end
 
+-- Synchronously organise (Go) imports. Taken from
+-- https://github.com/neovim/nvim-lsp/issues/115#issuecomment-654427197.
+function M.go_organize_imports_sync(timeout_ms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, 't', true } }
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/callbacks.lua) for how to do this properly.
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
+
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      vim.lsp.util.apply_workspace_edit(action.edit)
+    end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
+  end
+end
 
 return M
