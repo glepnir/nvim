42ba2cb212957f02690fd3ba0eab436caaacf75b
Raphael <glepnir@gopherhub.org>
2020-09-19 01:40:27 +0800

    refactor

--- lua/lsp/window.lua
+++ lua/lsp/window.lua
@@ -1,6 +1,9 @@
 local vim,api = vim,vim.api
 local M = {}
 
+-- 1 thin
+-- 2 radio
+-- 3 crude
 local border_style = {
   {top_left = "┌",top_mid = "─",top_right = "┐",mid = "│",bottom_left = "└",bottom_right= "┘" };
   {top_left = "╭",top_mid = "─",top_right = "╮",mid = "│",bottom_left = "╰",bottom_right= "╯" };
@@ -76,6 +79,30 @@ function M.create_float_window(contents,border)
   local contents_winid = api.nvim_open_win(contents_bufnr, true, opts)
   api.nvim_win_set_option(contents_winid,"winhl","Normal:LspNvim")
   return contents_bufnr,contents_winid,border_winid
+end
+
+function M.open_floating_preview(contents, filetype, opts)
+  opts = opts or {}
+
+  -- Clean up input: trim empty lines from the end, pad
+  contents = vim.lsp.util._trim_and_pad(contents, opts)
+
+  -- Compute size of float needed to show (wrapped) lines
+  opts.wrap_at = opts.wrap_at or (vim.wo["wrap"] and api.nvim_win_get_width(0))
+  local width, height = vim.lsp.util._make_floating_popup_size(contents, opts)
+
+  local floating_bufnr = api.nvim_create_buf(false, true)
+  if filetype then
+    api.nvim_buf_set_option(floating_bufnr, 'filetype', filetype)
+  end
+  local float_option = vim.lsp.util.make_floating_popup_options(width, height, opts)
+  local floating_winnr = api.nvim_open_win(floating_bufnr, false, float_option)
+  if filetype == 'markdown' then
+    api.nvim_win_set_option(floating_winnr, 'conceallevel', 2)
+  end
+  api.nvim_buf_set_lines(floating_bufnr, 0, -1, true, contents)
+  api.nvim_buf_set_option(floating_bufnr, 'modifiable', false)
+  return floating_bufnr, floating_winnr
 end
 
 return M
