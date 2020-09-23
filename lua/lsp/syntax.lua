d858c08fc9853f4eef243ad33783526b62b8601a
Raphael <glephunter@gmail.com>
2020-09-23 22:41:16 +0800

    fix window width

--- lua/lsp/syntax.lua
+++ lua/lsp/syntax.lua
@@ -7,15 +7,18 @@ function M.add_highlight()
   api.nvim_command("hi ReferencesCount guifg=#2e6ce8 gui=bold")
   api.nvim_command("hi DefinitionCount guifg=#2e6ce8 gui=bold")
   api.nvim_command("hi TargetFileName guifg=#a4e34b gui=bold")
-  api.nvim_command("hi ReferencesIcon guifg=#e3bc10")
-  api.nvim_command("hi HelpTitle guifg=red")
-  api.nvim_command("hi default link HelpItem Comment")
+  api.nvim_command("hi DefinitionIcon guifg=#c594c5")
+  api.nvim_command("hi ReferencesIcon guifg=#c594c5")
+  api.nvim_command("hi link HelpTitle Comment")
+  api.nvim_command("hi link HelpItem Comment")
   -- diagnostic
   api.nvim_command("hi DiagnosticTruncateLine guifg=#6699cc gui=bold")
   api.nvim_command("hi DiagnosticError guifg=#EC5f67 gui=bold")
   api.nvim_command("hi DiagnosticWarning guifg=#d8a657 gui=bold")
   api.nvim_command("hi DiagnosticInformation guifg=#6699cc gui=bold")
   api.nvim_command("hi DiagnosticHint guifg=#56b6c2 gui=bold")
+
+  api.nvim_command("hi DefinitionPreviewTitle guifg=#56b6c2 gui=bold")
 end
 
 return M
