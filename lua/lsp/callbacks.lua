8c6831bd9af09f30d4a8c47bbae3d380357faa27
Raphael <glephunter@gmail.com>
2020-09-25 14:36:05 +0800

    feat: add border float window for document

--- lua/lsp/callbacks.lua
+++ lua/lsp/callbacks.lua
@@ -1,4 +1,5 @@
 local vim,api,lsp = vim,vim.api,vim.lsp
+local window = require('lsp.window')
 local callbacks = {}
 
 local function lookup_section(settings, section)
@@ -9,65 +10,6 @@ local function lookup_section(settings, section)
     end
   end
   return settings
-end
-
---@private
-local function ok_or_nil(status, ...)
-  if not status then return end
-  return ...
-end
---@private
-local function npcall(fn, ...)
-  return ok_or_nil(pcall(fn, ...))
-end
---@private
-local function find_window_by_var(name, value)
-  for _, win in ipairs(api.nvim_list_wins()) do
-    if npcall(api.nvim_win_get_var, win, name) == value then
-      return win
-    end
-  end
-end
-
-local function focusable_float(unique_name, fn)
-  -- Go back to previous window if we are in a focusable one
-  if npcall(api.nvim_win_get_var, 0, unique_name) then
-    return api.nvim_command("wincmd p")
-  end
-  local bufnr = api.nvim_get_current_buf()
-  do
-    local win = find_window_by_var(unique_name, bufnr)
-    if win and api.nvim_win_is_valid(win) and not vim.fn.pumvisible() then
-      api.nvim_set_current_win(win)
-      api.nvim_command("stopinsert")
-      return
-    end
-  end
-  local pbufnr, pwinnr = fn()
-  if pbufnr then
-    api.nvim_win_set_var(pwinnr, unique_name, bufnr)
-    return pbufnr, pwinnr
-  end
-end
-
-local function focusable_preview(unique_name, fn)
-  return focusable_float(unique_name, function()
-    return vim.lsp.util.open_floating_preview(fn())
-  end)
-end
-
-local function signature_help_callback(_,method,result)
-  if not (result and result.signatures and result.signatures[1]) then
-    return
-  end
-  local lines = lsp.util.convert_signature_help_to_markdown_lines(result)
-  lines = lsp.util.trim_empty_lines(lines)
-  if vim.tbl_isempty(lines) then
-    return
-  end
-  focusable_preview(method, function()
-    return lines, lsp.util.try_trim_markdown_code_blocks(lines)
-  end)
 end
 
 -- Add I custom callbacks function in lsp server config
@@ -138,12 +80,25 @@ function callbacks.add_callbacks(server_setup)
     lsp.util.buf_diagnostics_signs(bufnr, result.diagnostics)
     api.nvim_command("doautocmd User LspDiagnosticsChanged")
   end
-  server_setup.callbacks["textDocument/signatureHelp"] = signature_help_callback
-end
 
-function callbacks.show_signature_help()
-  local params = vim.lsp.util.make_position_params()
-  return vim.lsp.buf_request(0,'textDocument/signatureHelp',params,signature_help_callback)
+  server_setup.callbacks['textDocument/hover'] = function(_, method, result)
+    vim.lsp.util.focusable_float(method, function()
+        if not (result and result.contents) then
+        -- return { 'No information available' }
+        return
+        end
+        local markdown_lines = lsp.util.convert_input_to_markdown_lines(result.contents)
+        markdown_lines = lsp.util.trim_empty_lines(markdown_lines)
+        if vim.tbl_isempty(markdown_lines) then
+        -- return { 'No information available' }
+        return
+        end
+        local bufnr,contents_winid,border_winid = window.fancy_floating_markdown(markdown_lines)
+        lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, contents_winid)
+        lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, border_winid)
+        return bufnr,contents_winid
+    end)
+    end
 end
 
 return callbacks
