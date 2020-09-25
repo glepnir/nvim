9ce1a4c240d2df2824e3a77ddd2f8385367ee3fc
Raphael <glepnir@gopherhub.org>
2020-09-25 15:27:40 +0800

    remake some colors

--- lua/lsp/syntax.lua
+++ lua/lsp/syntax.lua
@@ -2,13 +2,13 @@ local M = {}
 local api = vim.api
 
 function M.add_highlight()
-  api.nvim_command("hi LspFloatWinBorder guifg=#6699cc")
+  api.nvim_command("hi LspFloatWinBorder guifg=#c594c5")
   api.nvim_command("hi TargetWord guifg=#EC5f67 gui=bold")
   api.nvim_command("hi ReferencesCount guifg=#2e6ce8 gui=bold")
   api.nvim_command("hi DefinitionCount guifg=#2e6ce8 gui=bold")
-  api.nvim_command("hi TargetFileName guifg=#a4e34b gui=bold")
-  api.nvim_command("hi DefinitionIcon guifg=#c594c5")
-  api.nvim_command("hi ReferencesIcon guifg=#c594c5")
+  api.nvim_command("hi TargetFileName guifg=#c0c5ce gui=bold")
+  api.nvim_command("hi DefinitionIcon guifg=#d8a657")
+  api.nvim_command("hi ReferencesIcon guifg=#d8a657")
   api.nvim_command("hi link HelpTitle Comment")
   api.nvim_command("hi link HelpItem Comment")
   -- diagnostic
