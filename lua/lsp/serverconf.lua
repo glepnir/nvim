0cb7f06d422a60cb0bea5c46a9bfc061e3c36c5c
Raphael <glephunter@gmail.com>
2020-09-29 22:23:29 +0800

    add luagit to lua server

--- lua/lsp/serverconf.lua
+++ lua/lsp/serverconf.lua
@@ -27,6 +27,7 @@ server.lua = {
         enable = true,
         globals = {"vim"}
       },
+      runtime = {version = "LuaJIT"},
       workspace = {
         library = vim.list_extend({[vim.fn.expand("$VIMRUNTIME/lua")] = true},{}),
       },
