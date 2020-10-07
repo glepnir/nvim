a89bd145cccd8f1002da8c6b6eeef4f628f49f2b
Raphael <glephunter@gmail.com>
2020-10-07 13:21:33 +0800

    rename variable of quickrun

--- lua/quickrun.lua
+++ lua/quickrun.lua
@@ -1,23 +1,21 @@
 local vim = vim
-local M = {}
-
-do
-  M.go = {'go run ','go test '}
-  M.lua = {'lua '}
-end
+local M = {
+  go = {'go run ','go test '};
+  lua = {'lua '}
+}
 
 function M.run_command()
   local cmd = nil
-  local file_extension = vim.fn.expand("%:e")
+  local file_type = vim.fn.expand("%:e")
   local file_name = vim.fn.expand("%:p")
-  if file_extension == 'go' then
+  if file_type == 'go' then
     if file_name:match("_test") then
-      cmd = M[file_extension][2]
+      cmd = M[file_type][2]
     else
-      cmd = M[file_extension][1]
+      cmd = M[file_type][1]
     end
   else
-    cmd = M[file_extension][1]
+    cmd = M[file_type][1]
   end
   local output_list = vim.fn.split(vim.fn.system(cmd..file_name),'\n')
   for _,v in ipairs(output_list) do
