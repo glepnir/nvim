27089fb093e8bf1288bb025c131ad3a8c403f1a8
Raphael <glepnir@gopherhub.org>
2020-10-08 18:49:34 +0800

    support packer?

--- lua/core.lua
+++ lua/core.lua
@@ -1,9 +1,10 @@
 local global = require 'global'
 local options = require 'options'
 local autocmd = require 'event'
-local dein = require 'dein'
+-- local dein = require 'dein'
 local map = require 'mapping'
 local zephyr = require 'zephyr'
+local pack = require('pack')
 local vim = vim
 local M = {}
 
@@ -60,8 +61,10 @@ function M.load_core()
   local ops = options:new()
   ops:load_options()
 
-  local d = dein:new()
-  d:load_repos()
+  -- local d = dein:new()
+  -- d:load_repos()
+  local p = pack:new()
+  p.load_repos()
 
   map.load_mapping()
   autocmd.load_autocmds()
