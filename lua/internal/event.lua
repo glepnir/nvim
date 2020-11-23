c7ca29bbe623c9213b5a19893cdf75cd450d89f9
stephen <glepnir@gopherhub.org>
2020-11-23 15:00:20 +0800

    remove toml highlight plugin, cause treesitter support

--- lua/internal/event.lua
+++ lua/internal/event.lua
@@ -42,6 +42,7 @@ function autocmd.load_autocmds()
 
     ft = {
       {"FileType", "dashboard", "set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2"};
+      {"BufNewFile,BufRead","*.toml"," setf toml"},
     };
 
     yank = {
