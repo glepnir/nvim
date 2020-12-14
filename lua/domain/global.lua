637ae5f691319f4401ce3bb8c905b567a2785f83
stephen <glepnir@gopherhub.org>
2020-12-14 09:05:15 +0800

    rewrite saga

--- lua/domain/global.lua
+++ lua/domain/global.lua
@@ -1,11 +1,12 @@
 local global = {}
 local home    = os.getenv("HOME")
 local path_sep = global.is_windows and '\\' or '/'
+local os_name = vim.loop.os_uname().sysname
 
 function global:load_variables()
-  self.is_mac     = jit.os == 'OSX'
-  self.is_linux   = jit.os == 'Linux'
-  self.is_windows = jit.os == 'Windows'
+  self.is_mac     = os_name == 'Darwin'
+  self.is_linux   = os_name == 'Linux'
+  self.is_windows = os_name == 'Windows'
   self.vim_path    = home .. path_sep..'.config'..path_sep..'nvim'
   self.cache_dir   = home .. path_sep..'.cache'..path_sep..'vim'..path_sep
   self.modules_dir = self.vim_path .. path_sep..'modules'
