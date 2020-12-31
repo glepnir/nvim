35c25fb766311d3d1ca1c94d7d52d3fe7d27af90
stephen <glephunter@gmail.com>
2020-12-31 16:17:43 +0800

    add telescope dotfiles support

---
+++ lua/telescope/_extensions/dotfiles.lua
@@ -0,0 +1,33 @@
+local telescope = require('telescope')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local conf = require('telescope.config').values
+
+local dotfiles_list = function(opts)
+  local dir = opts.path or ''
+  local list = {}
+   local p = io.popen('find "'..dir..'" -type f')
+   for file in p:lines() do
+     if not file:match('.DS_Store') then
+      table.insert(list,file)
+     end
+   end
+   return list
+end
+
+local dotfiles = function(opts)
+  opts = opts or {}
+  local results = dotfiles_list(opts)
+
+  pickers.new(opts,{
+    prompt_title = 'find in dotfiles',
+    results_title = 'Dotfiles',
+    finder = finders.new_table {
+      results = results
+    },
+    previewer = conf.file_previewer(opts),
+    sorter = conf.file_sorter(opts)
+  }):find()
+end
+
+return telescope.register_extension { exports = {dotfiles = dotfiles} }
