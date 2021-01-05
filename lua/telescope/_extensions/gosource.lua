7db50879add011ade350f14656c764198c53bb8c
stephen <glepnir@gopherhub.org>
2021-01-05 09:19:24 +0800

    add gosource extensions for telescope

---
+++ lua/telescope/_extensions/gosource.lua
@@ -0,0 +1,42 @@
+local telescope = require('telescope')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local conf = require('telescope.config').values
+local os_name = vim.loop.os_uname().sysname
+local fn = vim.fn
+
+local golang_source = function()
+  local root
+  local result = {}
+  if os_name == 'Darwin' then
+    root = fn.globpath('/usr/local/Cellar/go', '*') .. '/libexec/src/'
+  end
+  local dicts = fn.split(fn.globpath(root, '*'))
+
+  for _,dict in pairs(dicts) do
+    local f = fn.split(fn.globpath(dict, '*.go'))
+    if next(f) ~= nil then
+      for _,val in pairs(f) do
+        table.insert(result,val)
+      end
+    end
+  end
+  return result
+end
+
+local gosource = function(opts)
+  opts = opts or {}
+  local results = golang_source()
+
+  pickers.new(opts,{
+    prompt_title = 'Find In Go Root',
+    results_title = 'Go Source Code',
+    finder = finders.new_table {
+      results = results
+    },
+    previewer = conf.file_previewer(opts),
+    sorter = conf.file_sorter(opts)
+  }):find()
+end
+
+return telescope.register_extension { exports = {gosource = gosource} }
