ef22df652274bd1388d3e5f48bf8fd4b98ea9cf5
bitcoder <glepnir@gopherhub.org>
2021-01-23 15:08:56 +0800

    update

--- lua/lspsaga/saga.lua
+++ lua/lspsaga/saga.lua
@@ -1,12 +1,13 @@
 local saga = {}
 
-function saga.init_lsp_saga()
+function saga.init_lsp_saga(opts)
+  opts = opts or {}
   local diagnostic = require 'lspsaga.diagnostic'
   local handlers = require 'lspsaga.handlers'
   local syntax = require 'lspsaga.syntax'
 
   handlers.overwrite_default()
-  diagnostic.lsp_diagnostic_sign()
+  diagnostic.lsp_diagnostic_sign(opts)
   syntax.add_highlight()
 end
 
