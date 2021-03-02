260778bc5a8a78e1242b1e753c9ab2b7476e21c1
stephen <glephunter@gmail.com>
2021-03-02 09:45:27 +0800

    use my hack packer

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -42,7 +42,7 @@ function Packer:load_packer()
   packer.reset()
   local use = packer.use
   self:load_plugins()
-  use {"glepnir/packer.nvim", opt = true}
+  use {"glepnir/packer.nvim", opt = true,branch= 'glepnir-patch-1'}
   for _,repo in ipairs(self.repos) do
     use(repo)
   end
