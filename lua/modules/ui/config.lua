2afcbcf975cb34aed9c0d82604c59c3f47cd37de
stephen <glephunter@gmail.com>
2021-03-08 09:22:40 +0800

    update

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -70,12 +70,34 @@ function config.nvim_tree()
   }
 end
 
-function config.vim_signify()
-  vim.g.signify_sign_add = '▋'
-  vim.g.signify_sign_change = '▋'
-  vim.g.signify_sign_delete = '▋'
-  vim.g.signify_sign_delete_first_line = '▘'
-  vim.g.signify_sign_show_count = 0
+function config._gitsigns()
+  require('gitsigns').setup {
+    signs = {
+      add = {hl = 'GitGutterAdd', text = '▋'},
+      change = {hl = 'GitGutterChange',text= '▋'},
+      delete = {hl= 'GitGutterDelete', text = '▋'},
+      topdelete = {hl ='GitGutterDeleteChange',text = '▔'},
+      changedelete = {hl = 'GitGutterChange', text = '▎'},
+    },
+    keymaps = {
+       -- Default keymap options
+       noremap = true,
+       buffer = true,
+
+       ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+       ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+
+       ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+       ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+       ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+       ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+       ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+       -- Text objects
+       ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+       ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+     },
+  }
 end
 
 return config
