25b1e96ac16717a13be581109455a3a11abe0666
stephen <glepnir@gopherhub.org>
2021-03-10 10:32:57 +0800

    update

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -5,6 +5,10 @@ ui['glepnir/zephyr-nvim'] = {
   config = [[vim.cmd('colorscheme zephyr')]]
 }
 
+ui['glepnir/dashboard-nvim'] = {
+  config = conf.dashboard
+}
+
 ui['glepnir/galaxyline.nvim'] = {
   branch = 'main',
   config = conf.galaxyline,
@@ -18,10 +22,6 @@ ui['glepnir/indent-guides.nvim'] = {
 ui['akinsho/nvim-bufferline.lua'] = {
   config = conf.nvim_bufferline,
   requires = {'kyazdani42/nvim-web-devicons'}
-}
-
-ui['glepnir/dashboard-nvim'] = {
-  config = conf.dashboard
 }
 
 ui['kyazdani42/nvim-tree.lua'] = {
