863aa756760f999a1a1b9ab9ae4065e12ce12be7
stephen <glephunter@gmail.com>
2021-03-11 09:43:41 +0800

    update

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -40,6 +40,7 @@ function config.telescope()
       prompt_prefix = '🍔 ',
       prompt_position = 'top',
       sorting_strategy = 'ascending',
+      results_width = 0.6,
       file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
       grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
       qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
