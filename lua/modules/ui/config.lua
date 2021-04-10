a002ebb2cc416dd75922a704f9087308bf48409f
bitcoder <glephunter@gmail.com>
2021-04-10 15:46:39 +0800

    update

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -71,7 +71,7 @@ function config.nvim_tree()
   }
 end
 
-function config._gitsigns()
+function config.gitsigns()
   if not packer_plugins['plenary.nvim'].loaded then
     vim.cmd [[packadd plenary.nvim]]
   end
@@ -102,6 +102,52 @@ function config._gitsigns()
        ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
      },
   }
+end
+
+function config.indent_blakline()
+  vim.g.indent_blankline_char = "│"
+  vim.g.indent_blankline_show_first_indent_level = true
+  vim.g.indent_blankline_filetype_exclude = {
+    "startify",
+    "dashboard",
+    "dotooagenda",
+    "log",
+    "fugitive",
+    "gitcommit",
+    "packer",
+    "vimwiki",
+    "markdown",
+    "json",
+    "txt",
+    "vista",
+    "help",
+    "todoist",
+    "NvimTree",
+    "peekaboo",
+    "git",
+    "TelescopePrompt",
+    "undotree",
+    "flutterToolsOutline",
+    "" -- for all buffers without a file type
+  }
+  vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
+  vim.g.indent_blankline_show_trailing_blankline_indent = false
+  vim.g.indent_blankline_show_current_context = true
+  vim.g.indent_blankline_context_patterns = {
+    "class",
+    "function",
+    "method",
+    "block",
+    "list_literal",
+    "selector",
+    "^if",
+    "^table",
+    "if_statement",
+    "while",
+    "for"
+  }
+  -- because lazy load indent-blankline so need readd this autocmd
+  vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
 end
 
 return config
--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -12,27 +12,30 @@ ui['glepnir/dashboard-nvim'] = {
 ui['glepnir/galaxyline.nvim'] = {
   branch = 'main',
   config = conf.galaxyline,
-  requires = {'kyazdani42/nvim-web-devicons'}
+  requires = 'kyazdani42/nvim-web-devicons'
 }
 
-ui['glepnir/indent-guides.nvim'] = {
+ui['lukas-reineke/indent-blankline.nvim'] = {
   event = 'BufRead',
+  branch = 'lua',
+  config = conf.indent_blakline
 }
+
 
 ui['akinsho/nvim-bufferline.lua'] = {
   config = conf.nvim_bufferline,
-  requires = {'kyazdani42/nvim-web-devicons'}
+  requires = 'kyazdani42/nvim-web-devicons'
 }
 
 ui['kyazdani42/nvim-tree.lua'] = {
   cmd = {'NvimTreeToggle','NvimTreeOpen'},
   config = conf.nvim_tree,
-  requires = {'kyazdani42/nvim-web-devicons'}
+  requires = 'kyazdani42/nvim-web-devicons'
 }
 
 ui['lewis6991/gitsigns.nvim'] = {
   event = {'BufRead','BufNewFile'},
-  config = conf._gitsigns,
+  config = conf.gitsigns,
   requires = {'nvim-lua/plenary.nvim',opt=true}
 }
 
