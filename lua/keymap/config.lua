adeeb0c1e0c290ef981035bc3ab9dfe59ad7add3
Raphael <glephunter@gmail.com>
2022-05-29 17:49:24 +0800

    fix

--- lua/keymap/config.lua
+++ lua/keymap/config.lua
@@ -1,10 +1,10 @@
 local function check_back_space()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
+  local col = vim.fn.col('.') - 1
+  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+      return true
+  else
+      return false
+  end
 end
 
 local t = function(str)
@@ -13,23 +13,27 @@ end
 
 --- move to prev/next item in completion menuone
 --- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
+_G.smart_tab = function()
+  local cmp = require('cmp')
+  local ok,luasnip = pcall(require,'luasnip')
+  if cmp.visible() and not ok then
     return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif ok and luasnip.expand_or_jumpable() then
+    return t "<Plug>luasnip-expand-or-jump"
   elseif check_back_space() then
     return t "<Tab>"
   else
-    return vim.fn['compe#complete']()
+    return t "<C-n>"
   end
 end
 
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
+_G.smart_shift_tab = function()
+  local cmp = require('cmp')
+  local luasnip = require('luasnip')
+  if cmp.visible() == 1 then
     return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
+  elseif luasnip.jumpable(-1) then
+    return t "<cmd>lua require'luasnip'.jump(-1)<CR>"
   else
     return t "<S-Tab>"
   end
