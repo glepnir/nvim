0c025652b7b4c298b853586984b53335c7db2a9c
Raphael <glephunter@gmail.com>
2022-06-03 17:04:08 +0800

    clean

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -33,14 +33,4 @@ function config.telescope()
     require'telescope'.load_extension('gosource')
 end
 
-function config.vim_cursorwod()
-  vim.api.nvim_command('augroup user_plugin_cursorword')
-  vim.api.nvim_command('autocmd!')
-  vim.api.nvim_command('autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0')
-  vim.api.nvim_command('autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif')
-  vim.api.nvim_command('autocmd InsertEnter * let b:cursorword = 0')
-  vim.api.nvim_command('autocmd InsertLeave * let b:cursorword = 1')
-  vim.api.nvim_command('augroup END')
-end
-
 return config
