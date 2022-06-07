c7eb26801f7c2c690b627daf1404648dedcb1b48
Raphael <glephunter@gmail.com>
2022-06-07 18:00:48 +0800

    fix wrong command

--- lua/modules/lang/plugins.lua
+++ lua/modules/lang/plugins.lua
@@ -3,7 +3,7 @@ local conf = require('modules.lang.config')
 
 lang['nvim-treesitter/nvim-treesitter'] = {
   event = 'BufRead',
-  run = 'TSUpdate',
+  run = ':TSUpdate',
   after = 'telescope.nvim',
   config = conf.nvim_treesitter,
 }
