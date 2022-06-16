86f49c0bf973c5049790a22230827753d0cffb59
Raphael <glephunter@gmail.com>
2022-06-16 17:54:25 +0800

    super tab for me

--- lua/keymap/config.lua
+++ lua/keymap/config.lua
@@ -1,10 +1,6 @@
-local function check_back_space()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-      return true
-  else
-      return false
-  end
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local t = function(str)
@@ -16,21 +12,27 @@ end
 _G.smart_tab = function()
   local cmp = require('cmp')
   local ok,luasnip = pcall(require,'luasnip')
-  if cmp.visible() and not ok then
+  local luasnip_status = false
+  if ok then
+    luasnip_status = luasnip.expand_or_jumpable()
+  end
+
+  if cmp.visible() and not luasnip_status then
     return t "<C-n>"
-  elseif ok and luasnip.expand_or_jumpable() then
+  elseif luasnip_status then
     return t "<Plug>luasnip-expand-or-jump"
-  elseif check_back_space() then
+  elseif has_words_before() then
     return t "<Tab>"
   else
-    return t "<C-n>"
+    return t "<Tab>"
   end
 end
 
 _G.smart_shift_tab = function()
   local cmp = require('cmp')
-  local luasnip = require('luasnip')
-  if cmp.visible() == 1 then
+  local _,luasnip = pcall(require,'luasnip')
+
+  if cmp.visible() then
     return t "<C-p>"
   elseif luasnip.jumpable(-1) then
     return t "<cmd>lua require'luasnip'.jump(-1)<CR>"
