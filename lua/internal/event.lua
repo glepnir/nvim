8aa99216464545af6f0358fcb1d1b0104e7f73f4
Raphael <glephunter@gmail.com>
2022-08-27 10:16:46 +0800

    update

--- lua/internal/event.lua
+++ lua/internal/event.lua
@@ -9,6 +9,14 @@ api.nvim_create_autocmd({ 'BufWritePre' }, {
   end,
 })
 
+api.nvim_create_autocmd('BufRead',{
+  group = my_group,
+  pattern = '*.conf',
+  callback = function()
+    api.nvim_buf_set_option(0,'filetype','conf')
+  end
+})
+
 api.nvim_create_autocmd('TextYankPost', {
   group = my_group,
   pattern = '*',
