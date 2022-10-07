cab122b586fd15ae5ec602203a7d2f91d85f573a
Raphael <glephunter@gmail.com>
2022-10-07 21:09:28 +0800

    clean the options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,25 +2,20 @@ local opt = vim.opt
 local cache_dir = os.getenv('HOME') .. '/.cache/nvim/'
 
 opt.termguicolors = true
-opt.mouse = 'nv'
 opt.hidden = true
-opt.fileformats = 'unix,mac,dos'
 opt.magic = true
 opt.virtualedit = 'block'
 opt.clipboard = 'unnamedplus'
 opt.wildignorecase = true
-opt.wildignore =
-  '.git,.hg,.svn,*.pyc,*.o,*.out,*.jpg,*.jpeg,*.png,*.gif,*.zip,**/tmp/**,*.DS_Store,**/node_modules/**,**/bower_modules/**'
 opt.backup = false
 opt.writebackup = false
 opt.swapfile = false
-opt.directory = cache_dir .. 'swag/'
+opt.directory = cache_dir .. 'swap/'
 opt.undodir = cache_dir .. 'undo/'
 opt.backupdir = cache_dir .. 'backup/'
 opt.viewdir = cache_dir .. 'view/'
 opt.spellfile = cache_dir .. 'spell/en.uft-8.add'
 opt.history = 2000
-opt.backupskip = '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim'
 opt.smarttab = true
 opt.shiftround = true
 opt.timeout = true
@@ -32,53 +27,48 @@ opt.redrawtime = 1500
 opt.ignorecase = true
 opt.smartcase = true
 opt.infercase = true
-opt.incsearch = true
-opt.wrapscan = true
-opt.grepprg = 'rg --hidden --vimgrep --smart-case --'
-opt.whichwrap = 'h,l,<,>,[,],~'
+
+if vim.fn.executable('rg') == 1 then
+  opt.grepformat = '%f:%l:%c:%m,%f:%l:%m'
+  opt.grepprg = 'rg --vimgrep --no-heading --smart-case'
+end
+
 opt.switchbuf = 'useopen'
-opt.diffopt = 'filler,iwhite,internal,algorithm:patience'
 opt.completeopt = 'menu,menuone,noselect'
 opt.jumpoptions = 'stack'
 opt.showmode = false
 opt.shortmess = 'aoOTIcF'
 opt.scrolloff = 2
 opt.sidescrolloff = 5
-opt.foldlevelstart = 99
 opt.ruler = false
 opt.list = true
 opt.showtabline = 0
-
+opt.winwidth = 30
 opt.pumheight = 15
-opt.helpheight = 12
 opt.showcmd = false
-opt.equalalways = false
-
 opt.cmdheight = 0
 opt.laststatus = 3
-
---wrap
-opt.showbreak = '↳  '
-opt.breakat = [[\ \	;:,!?]]
-opt.breakindentopt = 'shift:2,min:20'
-
 opt.listchars = 'tab:»·,nbsp:+,trail:·,extends:→,precedes:←'
 opt.pumblend = 10
 opt.winblend = 10
-
 opt.undofile = true
-opt.textwidth = 100
 opt.expandtab = true
 opt.autoindent = true
 opt.tabstop = 2
 opt.shiftwidth = 2
-opt.softtabstop = -1
-opt.wrap = false
+
+-- wrap
 opt.linebreak = true
+opt.whichwrap = 'h,l,<,>,[,],~'
+opt.breakindentopt = 'shift:2,min:20'
+opt.showbreak = '↳  '
+
 opt.number = true
-opt.colorcolumn = '100'
 opt.signcolumn = 'yes'
 opt.spelloptions = 'camel'
+
+opt.textwidth = 100
+opt.colorcolumn = '100'
 -- opt.conceallevel = 2
 -- opt.concealcursor = 'niv'
 
