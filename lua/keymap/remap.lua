a46ad1efc473c0522e3833e1e033ac7049c2b9c2
Raphael <glephunter@gmail.com>
2023-03-03 15:38:50 +0800

    update

--- lua/keymap/remap.lua
+++ lua/keymap/remap.lua
@@ -2,8 +2,8 @@ local map = require('core.keymap')
 local cmd = map.cmd
 
 map.n({
-  ['j'] = cmd('gj'),
-  ['k'] = cmd('gk'),
+  ['j'] = 'gj',
+  ['k'] = 'gk',
   ['<C-s>'] = cmd('write'),
   ['<C-x>k'] = cmd('bdelete'),
   ['<C-n>'] = cmd('bn'),
