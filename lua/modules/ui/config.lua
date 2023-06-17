5ce70aa61aad208e9c0e85e365180c09b334b92d
mathew <glepnir@neovim.pro>
2023-06-17 14:20:45 +0800

    update

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -51,7 +51,16 @@ function config.dashboard()
 end
 
 function config.gitsigns()
-  require('gitsigns').setup({})
+  require('gitsigns').setup({
+    signs = {
+      add = { text = '┃' },
+      change = { text = '┃' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked = { text = '┃' },
+    },
+  })
 end
 
 return config
