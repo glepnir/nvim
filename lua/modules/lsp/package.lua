ce1821d6b59bc5896e7f90f1a9692fbcaa1e395d
glepnir <glephunter@gmail.com>
2023-08-29 17:51:22 +0800

    update

--- lua/modules/lsp/package.lua
+++ lua/modules/lsp/package.lua
@@ -34,7 +34,7 @@ local function diag_config()
   }
   for _, type in ipairs(t) do
     local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = '⮞', texthl = hl, numhl = hl })
+    vim.fn.sign_define(hl, { text = '⯈', texthl = hl, numhl = hl })
   end
 
   vim.diagnostic.config({
