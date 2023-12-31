e60cd64307eb64608f6aa15690bfaf0a8286a2df
glepnir <glephunter@gmail.com>
2023-12-31 12:54:26 +0800

    update

--- lua/internal/event.lua
+++ lua/internal/event.lua
@@ -35,3 +35,10 @@ au('FileType', {
     end
   end,
 })
+
+--for alacritty only
+au('ExitPre', {
+  group = vim.api.nvim_create_augroup('Exit', { clear = true }),
+  command = 'set guicursor=a:ver90',
+  desc = 'Set cursor back to beam when leaving Neovim.',
+})
