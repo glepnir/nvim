9f1f34741a3fb3054eabd9ee86569b808835f6f8
glepnir <glephunter@gmail.com>
2024-05-02 22:02:16 +0800

    cpp is 4

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,5 +1,5 @@
 vim.opt_local.commentstring = '//%s'
 vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
