0788e0cd7c41f5398542f8e991b747d869f68757
glepnir <glephunter@gmail.com>
2024-05-03 17:44:06 +0800

    leave a todo

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,7 +1,11 @@
+local name = vim.api.nvim_buf_get_name(0)
+--when develop neovim project it use two space
+--othewise use 4
+local v = name and name:find('neovim') and 2 or 4
 vim.opt.commentstring = '//%s'
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = v
+vim.opt.softtabstop = v
+vim.opt.tabstop = v
 vim.opt.expandtab = true
