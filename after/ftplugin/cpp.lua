940ba1046a97ad731bb764ecac724935a0aa4549
glepnir <glephunter@gmail.com>
2024-06-03 16:01:38 +0800

    update

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,4 +1,3 @@
-vim.opt_local.commentstring = '//%s'
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,4 +1,4 @@
-vim.opt_local.commentstring = '//%s'
+vim.opt_local.commentstring = '// %s'
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.smartindent = true
