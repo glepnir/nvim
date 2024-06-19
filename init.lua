bfbceb4bb552c85d47cd75d2c45fdee76dd9b7e8
glepnir <glephunter@gmail.com>
2024-06-19 18:55:21 +0800

    update

--- init.lua
+++ init.lua
@@ -1,5 +1,40 @@
+local g = vim.g
 vim.loader.enable()
+--disable_distribution_plugins
+g.loaded_gzip = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_2html_plugin = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrwPlugin = 1
+g.mapleader = ' '
+
+-- the programming language which i write.
+g.my_program_ft = {
+  'c',
+  'cpp',
+  'rust',
+  'zig',
+  'go',
+  'lua',
+  'sh',
+  'python',
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'json',
+}
+
 -- Load Modules
 require('core')
-vim.cmd.colorscheme('pure')
 require('internal.event')
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,38 +1,3 @@
-local g = vim.g
---disable_distribution_plugins
-g.loaded_gzip = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_2html_plugin = 1
-g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
-g.loaded_netrwPlugin = 1
-g.mapleader = ' '
-
--- the programming language which i write.
-g.my_program_ft = {
-  'go',
-  'lua',
-  'sh',
-  'rust',
-  'c',
-  'cpp',
-  'zig',
-  'python',
-  'javascript',
-  'javascriptreact',
-  'typescript',
-  'typescriptreact',
-  'json',
-}
-
 require('core.pack'):boot_strap()
 require('core.options')
+vim.cmd.colorscheme('pure')
