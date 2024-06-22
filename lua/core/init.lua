020bfa85b009e46ed4e86accdac093450cfe8c11
glepnir <glephunter@gmail.com>
2024-06-22 15:32:54 +0800

    update

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,6 +1,8 @@
 require('core.pack'):boot_strap()
 require('core.options')
 
+-- some color i interested
+-- backup: #062329
 vim.cmd([[
 hi EndOfBuffer guifg=#14161b
 hi Function guifg=#a6dbff ctermfg=12
