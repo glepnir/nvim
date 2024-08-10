103439951884191231cc62224c8d8782e5d2f841
glepnir <glephunter@gmail.com>
2024-08-10 15:30:48 +0800

    update

--- lua/modules/lsp/backend.lua
+++ lua/modules/lsp/backend.lua
@@ -32,34 +32,30 @@ lspconfig.gopls.setup({
 })
 
 lspconfig.lua_ls.setup({
-  on_attach = M._attach,
-  capabilities = M.capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        unusedLocalExclude = { '_*' },
-        globals = { 'vim' },
-        disable = {
-          'luadoc-miss-see-name',
-          'undefined-field',
-        },
-      },
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
       runtime = {
         version = 'LuaJIT',
-        -- path = vim.split(package.path, ';'),
-      },
-      workspace = {
-        library = {
-          vim.env.VIMRUNTIME .. '/lua',
-          '${3rd}/busted/library',
-          '${3rd}/luv/library',
-        },
-        checkThirdParty = 'Disable',
       },
       completion = {
         callSnippet = 'Replace',
       },
-    },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          '${3rd}/luv/library',
+        },
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
   },
 })
 
