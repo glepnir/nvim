d5d5096a7619cdd263dc3eed81cd9055e08c526f
glepnir <glephunter@gmail.com>
2024-09-08 18:44:00 +0800

    update

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,12 +1,3 @@
 require('core.pack'):boot_strap()
 require('core.options')
 vim.cmd.colorscheme('solarized')
-local function on_list(options)
-  options.title = 'Reference: ' .. vim.fn.expand('<cword>')
-  vim.fn.setqflist({}, ' ', options)
-  vim.cmd.cfirst()
-end
-
-vim.keymap.set('n', '<leader>h', function()
-  vim.lsp.buf.references(nil, { on_list = on_list })
-end)
