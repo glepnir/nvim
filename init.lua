4c7b97ac28a9309469845cbc017c5c62920aa5d7
glepnir <glephunter@gmail.com>
2024-09-23 19:40:59 +0800

    clean

--- init.lua
+++ init.lua
@@ -1,23 +1,6 @@
 local g = vim.g
 vim.loader.enable()
---disable_distribution_plugins
-g.loaded_gzip = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_2html_plugin = 1
-g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
-g.loaded_netrwPlugin = 1
-g.mapleader = ' '
-
+g.mapleader = vim.keycode('<space>')
 -- the programming language which i write.
 _G.program_ft = {
   'c',
@@ -36,10 +19,26 @@ _G.program_ft = {
   'cmake',
   'html',
 }
-
 _G.is_mac = vim.uv.os_uname().sysname == 'Darwin'
 
 -- Load Modules
 require('core')
 require('internal.event')
 require('internal.completion')
+
+--disable_distribution_plugins
+g.loaded_gzip = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_2html_plugin = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrwPlugin = 1
