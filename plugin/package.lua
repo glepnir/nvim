866a77df5ee55da3a76a4e37bd3692c01450b7fa
glepnir <glephunter@gmail.com>
2025-04-22 20:11:48 +0800

    update

--- plugin/package.lua
+++ plugin/package.lua
@@ -175,92 +175,90 @@ async(function()
       },
     })
     :load_path(devpath)
+end)()
 
-  local i = '●'
-  vim.diagnostic.config({
-    virtual_text = { current_line = true },
-    signs = {
-      text = { i, i, i, i },
-    },
-  })
+local i = '●'
+vim.diagnostic.config({
+  virtual_text = { current_line = true },
+  signs = {
+    text = { i, i, i, i },
+  },
+})
 
-  api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-      local client = vim.lsp.get_clients({ id = args.data.client_id })[1]
-      client.server_capabilities.semanticTokensProvider = nil
-    end,
-  })
+api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_clients({ id = args.data.client_id })[1]
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
 
-  vim.lsp.config('lua_ls', {
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if
-          path ~= vim.fn.stdpath('config')
-          and (
-            vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')
-          )
-        then
-          return
-        end
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
       end
+    end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          version = 'LuaJIT',
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
         },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-          },
-        },
-      })
-    end,
-    settings = {
-      Lua = {},
-    },
-  })
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
 
-  vim.lsp.config('clangd', {
-    cmd = { 'clangd', '--background-index', '--header-insertion=never' },
-    init_options = { fallbackFlags = { vim.bo.filetype == 'cpp' and '-std=c++23' or nil } },
-  })
+vim.lsp.config('clangd', {
+  cmd = { 'clangd', '--background-index', '--header-insertion=never' },
+  init_options = { fallbackFlags = { vim.bo.filetype == 'cpp' and '-std=c++23' or nil } },
+})
 
-  vim.lsp.config('rust_analyzer', {
-    settings = {
-      ['rust-analyzer'] = {
-        imports = {
-          granularity = {
-            group = 'module',
-          },
-          prefix = 'self',
+vim.lsp.config('rust_analyzer', {
+  settings = {
+    ['rust-analyzer'] = {
+      imports = {
+        granularity = {
+          group = 'module',
         },
-        cargo = {
-          buildScripts = {
-            enable = true,
-          },
-        },
-        procMacro = {
+        prefix = 'self',
+      },
+      cargo = {
+        buildScripts = {
           enable = true,
         },
       },
+      procMacro = {
+        enable = true,
+      },
     },
-  })
+  },
+})
 
-  vim.lsp.enable({
-    'lua_ls',
-    'clangd',
-    'rust_analyzer',
-    'basedpyright',
-    'ruff',
-    'bashls',
-    'zls',
-    'cmake',
-    'jsonls',
-    'ts_ls',
-    'eslint',
-    'tailwindcss',
-    'cssls',
-  })
-end)()
+vim.lsp.enable({
+  'lua_ls',
+  'clangd',
+  'rust_analyzer',
+  'basedpyright',
+  'ruff',
+  'bashls',
+  'zls',
+  'cmake',
+  'jsonls',
+  'ts_ls',
+  'eslint',
+  'tailwindcss',
+  'cssls',
+})
