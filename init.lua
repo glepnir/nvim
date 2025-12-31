274255df006c8e080e7bdb447533278f06f9f7d6
glepnir <glephunter@gmail.com>
2025-12-31 14:39:59 +0800

    20251231-143959

--- init.lua
+++ init.lua
@@ -68,7 +68,7 @@ o.cot = 'menu,menuone,noinsert,fuzzy,nosort,popup'
 o.cia = 'kind,abbr,menu'
 vim.opt.guicursor:remove({ 't:block-blinkon500-blinkoff500-TermCursor' })
 
-vim.cmd.colorscheme('dicom')
+vim.cmd.colorscheme('solarized')
 g.health = { style = 'float' }
 g.editorconfig = false
 g._lang = {
