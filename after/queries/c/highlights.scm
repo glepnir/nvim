5b004466d967f0df10d3da626bad50442d79c69d
glepnir <glephunter@gmail.com>
2026-01-15 13:45:45 +0800

    20260115-134545

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -6,3 +6,6 @@
 ((identifier) @keyword.repeat
  (#match? @keyword.repeat "^FOR_ALL.*"))
 
+(function_definition
+  type: (type_identifier) @keyword.repeat
+  (#match? @keyword.repeat "^FOR_ALL_.*"))
