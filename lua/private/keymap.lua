27066b15ebd654994dc8928629247104eea47ec8
glepnir <glephunter@gmail.com>
2026-06-18 19:44:18 +0800

    20260618-194418

--- lua/private/keymap.lua
+++ lua/private/keymap.lua
@@ -385,5 +385,10 @@ map.nxo('[c', function()
 end, { desc = 'Prev comment' })
 
 map.c('<CR>', function()
+  local res = vim.fn.cmdcomplete_info()
+  if res.cmdline_orig == 'w' then
+    print('here')
+    return '<CR>'
+  end
   return vim.fn.pumvisible() == 1 and '<C-y><CR>' or '<CR>'
 end, { expr = true })
